dfcb8834e10874e13d520cb8152c38bfad760069
Alexey Nurgaliev <atnurgaliev@gmail.com>
2014-01-21 15:09:57 +0400

    Fix: vagrant provision script

--- provision.sh
+++ provision.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install -y ruby python-pip g++ make mc apache2
 sudo pip install Pygments
 sudo gem install jekyll
-sudo gem install RedCarpet
+sudo gem install RedCloth
 
 mkdir -p /vagrant/_site
 sudo rm -rf /var/www
