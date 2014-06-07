e9372e54d6d1ffa714166cbd31eb5551bad07dd4
Alexey Nurgaliev <atnurgaliev@gmail.com>
2014-06-07 10:29:04 +0400

    Параматры для webgl

--- js/experimental/webgl.js
+++ js/experimental/webgl.js
@@ -1,15 +1,64 @@
+var R = 15;
+var cube_num = 10;
+var cube_color = 'red';
+var rot_a = 0.002;
+var cam_rot_a = 0.0005;
+
+var cubes = [];
+var scene = new THREE.Scene();
+
+function createCube(x, y, z) {
+    var cubeGeometry = new THREE.BoxGeometry(4,4,4);
+    var cubeMaterial = new THREE.MeshLambertMaterial(
+        {color: cube_color});
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+    cube.position.x = x;
+    cube.position.y = y;
+    cube.position.z = z;
+
+    cube.castShadow = true;
+    cube.receiveShadow = true;
+
+    scene.add(cube);
+    cubes.push(cube);
+}
+
+function createAllCubes() {
+    cubes = [];
+    for(var i = 0; i < cube_num; i++) {
+        var x = R * Math.cos(2 * Math.PI / cube_num * i);
+        var z = R * Math.sin(2 * Math.PI / cube_num * i);
+        createCube(x, 0, z);
+    }
+}
+
+function removeCubes() {
+    for(var i = 0; i < cube_num; i++) {
+        scene.remove(cubes[i]);
+    }
+}
+
+function rotate(obj, angle) {
+    var x = obj.position.x;
+    var z = obj.position.z;
+
+    var x1 = x * Math.cos(angle) - z * Math.sin(angle);
+    var z1 = x * Math.sin(angle) + z * Math.cos(angle);
+
+    obj.position.x = x1;
+    obj.position.z = z1;
+}
+
 $(function() {
     var height = 400;
     var width = 400;
 
-    var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(35
         , width / height , 0.1, 1000);
 
     var renderer = new THREE.WebGLRenderer();
-    renderer.setClearColorHex(0xEEEEEE);
-    renderer.setSize(width, height);
-    renderer.setClearColorHex(0xEEEEEE, 1.0);
+    renderer.setClearColor(0xEEEEEE);
     renderer.setSize(width, height);
     renderer.shadowMapEnabled = true;
 
@@ -23,32 +72,7 @@ $(function() {
     plane.receiveShadow = true;
     scene.add(plane);
 
-    var cubes = [];
-
-    function createCube(x, y, z) {
-        var cubeGeometry = new THREE.CubeGeometry(4,4,4);
-        var cubeMaterial = new THREE.MeshLambertMaterial(
-            {color: 0xff0000});
-        var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-        cube.position.x = x;
-        cube.position.y = y;
-        cube.position.z = z;
-
-        cube.castShadow = true;
-        cube.receiveShadow = true;
-
-        scene.add(cube);
-        cubes.push(cube);
-    }
-
-    var R = 15;
-    var cube_num = 10;
-    for(var i = 0; i < cube_num; i++) {
-        var x = R * Math.cos(2 * Math.PI / cube_num * i);
-        var z = R * Math.sin(2 * Math.PI / cube_num * i);
-        createCube(x, 0, z);
-    }
+    createAllCubes();
 
     var sphereGeometry = new THREE.SphereGeometry(8,40,20);
     var sphereMaterial = new THREE.MeshLambertMaterial(
@@ -67,7 +91,7 @@ $(function() {
     var spotLight = new THREE.SpotLight( 0xffffff );
     spotLight.position.set( -40, 60, -10 );
     spotLight.castShadow = true;
-    scene.add(spotLight );
+    scene.add(spotLight);
 
     $("#output").append(renderer.domElement);
 
@@ -86,26 +110,9 @@ $(function() {
             cube.rotation.x += 0.02;
             cube.rotation.y += 0.02;
             cube.rotation.z += 0.02;
-
-            var a = 0.02;
-            var x = cube.position.x;
-            var z = cube.position.z;
-            var x1 = x * Math.cos(a) - z * Math.sin(a);
-            var z1 = x * Math.sin(a) + z * Math.cos(a);
-
-            cube.position.x = x1;
-            cube.position.z = z1;
+            rotate(cubes[i], rot_a);
         }
-        
-        var a = 0.001;
-        var x = camera.position.x;
-        var z = camera.position.z;
-        var x1 = x * Math.cos(a) - z * Math.sin(a);
-        var z1 = x * Math.sin(a) + z * Math.cos(a);
-
-        camera.position.x = x1;
-        camera.position.z = z1;
-
+        rotate(camera, cam_rot_a);
         camera.lookAt(sphere.position);
 
         requestAnimationFrame(renderScene);
@@ -113,4 +120,18 @@ $(function() {
     }
 
     renderScene();
+});
+
+$(function() {
+    $('#apply').click(function() {
+        removeCubes();
+
+        cube_num = parseInt($('#cubes').val()) || 10;
+        cube_color = $('#cube-color').val();
+        R = parseInt($('#radius').val()) || 15;
+        rot_a = parseFloat($('#cube-speed').val()) || 0.002;
+        cam_rot_a = parseFloat($('#scene-speed').val()) || 0.0005;
+
+        createAllCubes();
+    });
 });
