7bbc6a386ea39facdf152aae90e06c50f2660474
gvalentiny <gvalentiny@gmail.com>
2017-07-11 20:52:19 +0400

    Правка генерирования случаных последовательностей символов

--- js/random.js
+++ js/random.js
@@ -1,10 +1,19 @@
-function start_random() {
-	var str, rez;
-	for (var i = 'a'; i <= 'z'; ++i) str += i;
-	for (var i = 'A'; i <= 'Z'; ++i) str += i;
-	for (var i = '0'; i <= '9'; ++i) str += i;
-	str += "!@#$%^&*()_+*/\\|,.?{}[]:;'\"<>";
+ function start_random() {
+	var arr = [ 
+		"abcdefghijklmnopqrstuvwxyz", 
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ", 
+		"0123456789",
+		"!@#$%^&*()_+*/\\|,.?{}[]:;'\"<>"
+	];
+	
+	var str = "";
+	str += arr[0];
+	str += arr[1];
+	str += arr[2];
+	str += arr[3];
+	
+	var rez = "";
 	for(var i = 0; i < 10; ++i)
-		rez += str[Math.floor(str.length() * Math.random())];
+		rez += str[Math.floor(str.length * Math.random())];
 	document.getElementById('text').value = rez;
 }
